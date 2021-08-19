3b154959d174a62b8618c9f370828449d33f6d54
ButerD <74465124+ButerD@users.noreply.github.com>
2021-08-19 11:18:21 +0300

    init

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Main {
+    private final static String URL = "jdbc:mysql://localhost:3306/test";
+    private final static String USER = "root";
+    private final static String PASSW = "12345";
+
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSW)) {
+            PreparedStatement ps = connection.prepareStatement("select * from users");
+
+
+
+        } catch (SQLException e) {
+
+        }
     }
 }
